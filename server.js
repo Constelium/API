b41964a57256af14e69a78435e7ea8b8e0d8224f
Issou83 <issou8@gmail.com>
2024-02-12 22:28:24 +0100

    Test des CORS

--- server.js
+++ server.js
@@ -12,7 +12,19 @@ require("dotenv").config();
 
 connectDB();
 
-app.use(cors()); // Utilisez cors comme middleware
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.use(express.json());
 
 app.use("/user", userRoutes);
